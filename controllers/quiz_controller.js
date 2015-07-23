066957806b6226c07e59c04537ca479050cbc8dd
Roberto Carrillo Garcia <rober_cg@telefonica.net>
2015-07-23 19:44:49 +0200

    #03 primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,16 @@
+// controller para los quiz question y anbswer
+
+// GET /quizes/question
+exports.question = function(req, res) {
+  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  if ( req.query.respuesta === 'Roma') {
+    res.render('quizes/answer', {respuesta: 'Correcto'});
+  }
+  else {
+    res.render('quizes/answer', {respuesta: 'Incorrecto'});
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Importamos el controller para las quizes
+// Contendrá los rutas a question y answer
+var quizController = require ('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// GET quizes/question a su controller
+router.get('/quizes/question', quizController.question);
+// GET quizes/answer a su controller
+router.get('/quizes/answer',   quizController.answer  );
 
 module.exports = router;
